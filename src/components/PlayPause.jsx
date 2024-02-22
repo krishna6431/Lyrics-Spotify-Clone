256d990baf48f6ec13df632f9e2a35b7a05009f6
krishna6431 <vermakrish921@gmail.com>
2024-02-23 01:12:28 +0530

    create PLay Pause Component

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,28 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+const PlayPause = ({
+  isPlaying,
+  activeSong,
+  song,
+  handlePause,
+  handlePlay,
+}) => {
+  if (isPlaying && activeSong.title === song.title) {
+    return (
+      <FaPauseCircle
+        size={35}
+        className="text-gray-300"
+        onClick={handlePause}
+      ></FaPauseCircle>
+    );
+  } else {
+    return (
+      <FaPlayCircle
+        size={35}
+        className="text-gray-300"
+        onClick={handlePlay}
+      ></FaPlayCircle>
+    );
+  }
+};
 
 export default PlayPause;
